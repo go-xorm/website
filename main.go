5f9773564867a5e6ab2ce89974166190c3f7eb2f
Lunny Xiao <xiaolunwen@gmail.com>
2014-07-08 09:14:24 +0800

    add gocms

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Unknwon/i18n"
 	"github.com/go-xweb/log"
 	"github.com/go-xweb/xweb"
@@ -27,5 +28,6 @@ func main() {
 	xweb.AddAction(&actions.HomeAction{})
 	xweb.AutoAction(&actions.DocsAction{})
 	xweb.AddTmplVar("i18n", i18n.Tr)
-	xweb.Run(":9999")
+	port, _ := models.Cfg.GetValue("app", "http_port")
+	xweb.Run(fmt.Sprintf(":%v", port))
 }
